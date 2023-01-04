48c36ab4e3e68240a7504001225095da81a44d1f
Demetrio Aris <gon21010@byui.edu>
2023-01-04 15:59:10 -0500

    Finish the program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,40 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please introduce your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+    
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grades is {letter}");
+
+        if (grade >= 70)
+        {
+          Console.WriteLine("Congratulation, you passed the courses!");
+        }
+        else
+        {
+          Console.WriteLine("Good try, better luck next time!");
+        }
     }
 }
