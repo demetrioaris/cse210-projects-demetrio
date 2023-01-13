3dcb61e5c41b9befc25a310f7b17a2413a833d1e
Demetrio-Aris <85774830+Demetrio-Aris@users.noreply.github.com>
2023-01-12 22:22:27 -0500

    finished the program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess My Number game");
+        Console.WriteLine("***");
+
+        //Console.WriteLine("What is the magic number? ");
+        //string magicNumbInput = Console.ReadLine();
+        //int magicNumb = int.Parse(magicNumbInput);
+        //Console.WriteLine();
+        Random randomGenerator = new Random();
+        int magicNumb = randomGenerator.Next(1, 100);
+        
+        int guessNumb ;
+        do{
+            Console.WriteLine("What is your guess? ");
+            string guessNumbInput = Console.ReadLine();
+            guessNumb = int.Parse(guessNumbInput);
+
+            if (guessNumb == magicNumb)
+            {
+            Console.WriteLine("You Guessed it!!!");
+            }
+            else if (guessNumb > magicNumb)
+            {
+            Console.WriteLine("Higher");
+            }
+            else
+            {
+            Console.WriteLine("Lower");
+            }
+        }
+        
+        while (guessNumb != magicNumb);
     }
 }
