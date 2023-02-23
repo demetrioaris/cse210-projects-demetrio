ac67ea12a6dfc3688818271eeb7161fd7f13c290
Demetrio Aris <gon21010@byui.edu>
2023-02-22 21:39:27 -0500

    Complete W09 Prepare: Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("red", 4);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("blue", 6, 4);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("green", 4);
+        shapes.Add(c1);
+        
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"Color: {color} --> Area: {area}");
+        }
     }
 }
